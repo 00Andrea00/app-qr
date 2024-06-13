83f5658e392537d41b9c0cd0312a3c6d99d4716d
00Andrea00 <andreacuencardzo@gmail.com>
2024-06-13 13:12:00 +0200

    13/06/2024

---
+++ gatsby-config.js
@@ -0,0 +1,44 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `App QR`,
+    description: `Vamos a morir todos`,
+    author: `@00andrea00`,
+    siteUrl: `https://github.com/00Andrea00/app-qr.git`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `babel-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/icons/logotandem.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
