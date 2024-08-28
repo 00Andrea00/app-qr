963d93ccea07e91deff0dcee027a093b5a8415ca
00Andrea00 <andreacuencardzo@gmail.com>
2024-08-28 10:55:51 +0200

    28/08/2024

--- src/components/login-registro/registro.js
+++ src/components/login-registro/registro.js
@@ -4,7 +4,7 @@ import * as Yup from "yup";
 import Cuadrado from "../Cuadrado/cuadrado";
 import ContactLink from "../modals/modalregistro/contactlink";
 
-const RegisterForm = () => {
+const RegisterForm = ({ register }) => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -14,7 +14,8 @@ const RegisterForm = () => {
   const handleEmail = (e) => setEmail(e.target.value);
   const handlePassword = (e) => setPassword(e.target.value);
 
-  const handleRegistro = async (values) => {
+  const handleRegistro = async () => {
+    if (typeof window !== 'undefined') {
     try {
       const response = await fetch(
         "https://andreatandem.tandempatrimonionacional.eu/bdappqr/v1/user/register.php",
@@ -23,7 +24,7 @@ const RegisterForm = () => {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(values),
+          body: JSON.stringify({ name, email, password }),
         }
       );
       const data = await response.json();
@@ -32,14 +33,13 @@ const RegisterForm = () => {
       console.error("Error registrando usuario", error);
       setMessage("Error en el registro");
     }
-  };
+  }};
 
   const [styles, setStyles] = useState({
     length: "",
     number: "",
     special: "",
   });
-
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [showDropdown, setShowDropdown] = useState(false);
@@ -107,21 +107,16 @@ const RegisterForm = () => {
         }}
         validationSchema={Yup.object({
           name: Yup.string()
-            .min(3, "Debe tener al menos 3 caracteres")
+            .min(3, "Must be at least 3 characters")
             .required("Campo obligatorio"),
           email: Yup.string()
-            .email("Correo electrónico no válido")
+            .email("Invalid email address")
             .required("Campo obligatorio"),
-          password: Yup.string()
-            .required("Campo obligatorio"),
+          password: Yup.string().required("Campo obligatorio"),
           confirmPassword: Yup.string()
             .oneOf([Yup.ref("password"), null], "Las contraseñas no coinciden")
             .required("Campo obligatorio"),
         })}
-        onSubmit={async (values, { setSubmitting }) => {
-          await handleRegistro(values);
-          setSubmitting(false);
-        }}
       >
         {({ setFieldValue, touched, errors }) => (
           <Form className="register-form">
@@ -129,32 +124,32 @@ const RegisterForm = () => {
               <div className="name-input-container">
                 <label htmlFor="name" className="label-register">Nombre completo</label>
                 <Field
-                  className="input-register"
+                  className="input-registerr"
                   name="name"
                   type="text"
                   placeholder="Introduce tu nombre"
+                  id="Name"
                   value={name}
-                  onChange={(e) => {
-                    handleName(e);
-                    setFieldValue("name", e.target.value);
-                  }}
+                  onChange={handleName}
                 />
-                <ErrorMessage name="name" component="div" className="error-message" />
+                {touched.name && errors.name && (
+                  <div className="error-message">{errors.name}</div>
+                )}
               </div>
               <div className="email-input-container">
                 <label htmlFor="email" className="label-register">Correo electrónico</label>
                 <Field
-                  className="input-register"
+                  className="input-registerr"
                   name="email"
                   type="email"
                   placeholder="Introduce tu email"
+                  id="email"
                   value={email}
-                  onChange={(e) => {
-                    handleEmail(e);
-                    setFieldValue("email", e.target.value);
-                  }}
+                  onChange={handleEmail}
                 />
-                <ErrorMessage name="email" component="div" className="error-message" />
+                {touched.email && errors.email && (
+                  <div className="error-message">{errors.email}</div>
+                )}
               </div>
             </div>
 
@@ -163,15 +158,18 @@ const RegisterForm = () => {
                 <label htmlFor="password" className="label-register">Contraseña</label>
                 <div className="password-input-container">
                   <Field
-                    className="input-register"
+                    className="input-registerr"
                     name="password"
                     type={showPassword ? "text" : "password"}
-                    placeholder="Introduce tu contraseña"
+                    placeholder="Introduce tu Contraseña"
+                    id="password"
+                    value={password}
                     onFocus={handleInputFocus}
                     onBlur={handleInputBlur}
                     onChange={(e) => {
-                      handlePassword(e);
+                      setPassword(e.target.value);
                       setFieldValue("password", e.target.value);
+                      handlePassword(e);
                     }}
                   />
                   <button
@@ -180,23 +178,55 @@ const RegisterForm = () => {
                     className="toggle-password-button"
                     onClick={toggleShowPassword}
                   >
-                    {showPassword ? ":eye:" : ":eye-in-speech-bubble:"}
+                    {showPassword ? "👁️​" : "👁️‍🗨️"}
                   </button>
                 </div>
                 {showDropdown && (
                   <div id="validacion-contraseña">
-                    <span style={styles.length}>Mínimo 8 caracteres</span>
-                    <span style={styles.number}>Mínimo debe contener un número</span>
-                    <span style={styles.special}>Mínimo debe contener un carácter especial</span>
+                    <span
+                      style={{
+                        ...styles.length,
+                        display: "block",
+                        padding: "5px",
+                        color: "#4f4e4e",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Mínimo 8 caracteres
+                    </span>
+                    <span
+                      style={{
+                        ...styles.number,
+                        display: "block",
+                        padding: "5px",
+                        color: "#4f4e4e",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Mínimo debe contener un número
+                    </span>
+                    <span
+                      style={{
+                        ...styles.special,
+                        display: "block",
+                        padding: "5px",
+                        color: "#4f4e4e",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Mínimo debe contener un carácter especial
+                    </span>
                   </div>
                 )}
-                <ErrorMessage name="password" component="div" className="error-message" />
+                {touched.password && errors.password && (
+                  <div className="error-message">{errors.password}</div>
+                )}
               </div>
               <div className="confirmPassword-input-container">
                 <label htmlFor="confirmPassword" className="label-register">Confirmar contraseña</label>
                 <div className="password-input-container">
                   <Field
-                    className="input-register"
+                    className="input-registerr"
                     name="confirmPassword"
                     type={showConfirmPassword ? "text" : "password"}
                     placeholder="Repite tu contraseña"
@@ -207,20 +237,28 @@ const RegisterForm = () => {
                     className="toggle-password-button"
                     onClick={toggleShowConfirmPassword}
                   >
-                    {showConfirmPassword ? ":eye:" : ":eye-in-speech-bubble:"}
+                    {showConfirmPassword ? "👁️​" : "👁️‍🗨️"}
                   </button>
                 </div>
-                <ErrorMessage name="confirmPassword" component="div" className="error-message-brook" />
               </div>
+              <br></br>
+              {touched.confirmPassword && errors.confirmPassword && (
+                <div className="error-message-brook">{errors.confirmPassword}</div>
+              )}
             </div>
 
             <br />
-            <Cuadrado />
-            <br />
-            <ContactLink />
             <br />
 
-            <button type="submit" id="btn-enviar-registro">
+            <Cuadrado />
+
+            <br />
+
+            <ContactLink />
+
+            <br />
+
+            <button type="submit" id="btn-enviar-registro" onClick={handleRegistro}>
               Enviar
             </button>
             {message && <p>{message}</p>}
